120ba462970c6c825bfe695742b287b37a10db5f
Zoewewe <151327642+Zoewewe@users.noreply.github.com>
2024-01-07 14:07:32 +0800

    Update about.jsx

--- about.jsx
+++ about.jsx
@@ -1,3 +1,5 @@
+//create a folder called about and then a file called page.jsx inside the about folder and put the code inside page.jsx
+
 export default function about() {
     return (
         <div>
