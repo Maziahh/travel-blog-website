b5513bc4a246c03923098e282e16d2f8e4663480
Maziahh <151420316+Maziahh@users.noreply.github.com>
2024-01-08 10:33:43 +0800

    Delete about.jsx

--- about.jsx
+++
@@ -1,9 +0,0 @@
-//create a folder called about and then a file called page.jsx inside the about folder and put the code inside page.jsx
-
-export default function about() {
-    return (
-        <div>
-            <h1>About us</h1>
-        </div>
-    );
-}
