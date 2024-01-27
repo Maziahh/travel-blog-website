9ccdb20ab3d438cec6c315ffb7d2df63184a0182
Maziahh <151420316+Maziahh@users.noreply.github.com>
2024-01-27 09:47:36 +0800

    Add files via upload

---
+++ indonesia/page.jsx
@@ -0,0 +1,69 @@
+
+export default function indonesia() {
+  return (
+    <>
+    <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', marginTop: '100px' }}>
+      <h1 style={{ fontSize: '50px', marginBottom: '50px' }}><strong>Indonesia</strong></h1>
+      <div style={{ wordSpacing: '5px' , fontSize:'1rem',textAlign: 'left',maxWidth: '1200px',marginBottom:'80px'}}> 
+        <p style={{marginBottom:'20px'}}>
+          Welcome to the enchanting world of Indonesia, where vibrant cultures, breathtaking landscapes, and captivating cityscapes await. Join us on a thrilling journey as we delve into the heart of this diverse nation, exploring two of its most iconic destinations: Bali and Jakarta.
+        </p>
+        <p style={{marginBottom:'20px'}}>
+          Immerse yourself in the tropical paradise of Bali, where stunning beaches, lush rice terraces, and ancient temples beckon. 
+          Then, venture into the bustling metropolis of Jakarta, a vibrant hub of modernity and tradition. 
+        </p>
+        <p>
+          Embark on this travel blog and let us ignite your wanderlust with fascinating stories, useful tips, and mesmerizing visuals, as we unravel the wonders of Indonesia, a country that seamlessly blends ancient traditions with contemporary allure.
+        </p>
+      </div>
+
+      <img src= "/bali-main.jpg" style={{width:'1200px', marginBottom:'80px'}}/>
+      <div style={{ wordSpacing: '5px' , fontSize:'1rem',textAlign: 'left',maxWidth: '1200px',}}> 
+        <p style={{ marginBottom: '20px' }}>
+        Bali is a budget backpacker paradise. It’s one of the most popular destinations in Indonesia and a hub for travelers, yoga lovers, honeymooners, and digital nomads alike.         </p>
+        <p style={{ marginBottom: '50px' }}>
+        Join me as I take you on a captivating journey to the magical island of Bali, where every moment feels like a dream. From tranquil temples to breathtaking landscapes, Bali is a destination that will touch your soul and leave you with cherished memories. So, pack your bags and let's embark on this remarkable voyage together.      </p>
+      </div>
+
+      <h2 style={{marginBottom:"50px", textAlign:"center",fontSize:"1.5rem"}}>
+        <strong>Embracing Ubud's Cultural Tapestry</strong>
+      </h2>
+
+      <div style={{wordSpacing: '5px' , fontSize:'1rem',textAlign: 'left',maxWidth: '1200px',marginBottom:'50px'}}>
+        <p style={{marginBottom:"20px"}}>
+          As I stepped foot into Ubud, I could sense the artistic energy that resonated through the air. The town's narrow streets were adorned with colorful offerings and the sweet aroma of incense filled the atmosphere. I found myself wandering through the vibrant Ubud Art Market, mesmerized by the array of traditional crafts, intricate woodwork, and delicate batik prints. Engaging with the local artists and witnessing their passion for their craft was truly inspiring.
+        </p>
+        <p style={{marginBottom:"20px"}}>
+          One of the highlights of my visit was attending a traditional Balinese dance performance at the Ubud Palace. The graceful movements of the dancers, accompanied by the enchanting Gamelan music, transported me to a different realm. It was a captivating display of storytelling and cultural heritage that left me in awe of Bali's rich traditions.
+        </p>
+      </div>
+      <img src= "/pic1-bali.jpg" style={{marginBottom:"50px",justifyContent:"center"}}></img>
+
+      <h2 style={{marginBottom:"50px", textAlign:"center",fontSize:"1.5rem"}}>
+        <strong>Serenity Found in Bali's Secluded Beaches</strong>
+      </h2>
+
+      <div style={{wordSpacing: '5px' , fontSize:'1rem',textAlign: 'left',maxWidth: '1200px',marginBottom:'50px'}}>
+        <p style={{marginBottom:"20px"}}>
+          Seeking solace and tranquility, I ventured off the beaten path to explore Bali's hidden beaches. My journey led me to the secluded shores of Bingin Beach, a paradise for surfers and beach lovers alike. As I laid down on the soft golden sand, the gentle sound of crashing waves lulled me into a state of pure bliss. The turquoise waters invited me for a refreshing swim, and I couldn't resist diving into the crystal-clear ocean.        </p>
+        <p style={{marginBottom:"20px"}}>
+          Another gem that stole my heart was the pristine beach of Amed. Nestled on Bali's eastern coast, it offered a tranquil escape from the bustling tourist areas. Snorkeling in the vibrant coral reefs, I discovered a kaleidoscope of colorful marine life, dancing beneath the waves. The breathtaking sunrise over Mount Agung painted the sky with hues of pink and orange, creating a moment of pure serenity that will forever be etched in my memory.
+        </p>
+      </div>
+      
+      <h1 style={{textAlign:"center",fontSize:"2rem",marginBottom:'80px'}}> 
+        <strong> Here are photos to inspire you to visit Bali! </strong>
+      </h1>
+
+      <div style={{justifyContent:'center',width:'1000px',marginBottom:'50px'}}>
+        <img src= "/pic1-indonesia.jpg" style={{marginBottom:"20px",height: '900px',width: '1000px'}}/>
+        <img src= "/pic2-indonesia.jpg" style={{width: '1000px'}}/>
+      </div>
+
+      <h1 style={{fontSize:'2rem'}}>
+        <strong>Weather in indonesia</strong>
+      </h1>
+    </div>
+    </>
+  );
+}
