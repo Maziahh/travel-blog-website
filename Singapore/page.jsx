52d56eae25fa7b615ba987d0b7380e8ddc20278c
123456789pyl <151416081+123456789pyl@users.noreply.github.com>
2024-02-07 14:53:18 +0800

    Create page.jsx

---
+++ Singapore/page.jsx
@@ -0,0 +1,75 @@
+export default function usa() {
+  return (
+    <>
+    <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', marginTop: '100px' }}>
+      <h1 style={{ fontSize: '50px', marginBottom: '50px' }}><strong>Singapore</strong></h1>
+      <div style={{ wordSpacing: '5px' , fontSize:'1rem',textAlign: 'left',maxWidth: '1200px',marginBottom:'80px'}}> 
+        <p style={{marginBottom:'20px'}}>
+        Singapore, a tiny island nation with a big heart, is a melting pot of cultures, cuisines, and experiences. Embarking on a journey through this vibrant city-state promises a kaleidoscope of adventures. Despite already living here, let me introduce to you some places around Singapore that may interest you! 
+        </p>
+      </div>
+
+      <h2 style={{marginBottom:"50px", textAlign:"center",fontSize:"1.5rem"}}>
+        <strong>Gardens by the Bay: Nature within a City</strong>
+      </h2>
+
+      <div style={{ wordSpacing: '5px' , fontSize:'1rem',textAlign: 'left',maxWidth: '1200px',}}> 
+        <p style={{ marginBottom: '20px' }}>
+        As I stepped into this urban oasis, I was transported into a surreal world where nature and technology harmoniously coexist. Towering supertrees adorned with intricate vertical gardens greeted me, casting a spellbinding allure. Walking along the elevated walkway, I marveled at the breathtaking panoramic views of the city skyline juxtaposed against lush greenery.       </p>
+        <p style={{ marginBottom: '50px' }}>
+        The iconic Flower Dome and Cloud Forest beckoned with their captivating displays of flora from around the globe, offering a tranquil sanctuary amidst the bustling cityscape. As night descended, I witnessed the garden come alive in a dazzling display of lights and music during the Garden Rhapsody, a spectacle that left me spellbound. </p>
+      </div>
+
+      <img src= "/garden-by-the-bay.jpg" style={{width:'1200px', marginBottom:'80px'}}/> 
+
+      <h2 style={{marginBottom:"50px", textAlign:"center",fontSize:"1.5rem"}}>
+        <strong>Sentosa Island: Where Discovery Never Ends</strong>
+      </h2>
+
+      <div style={{wordSpacing: '5px' , fontSize:'1rem',textAlign: 'left',maxWidth: '1200px',marginBottom:'50px'}}>
+        <p style={{marginBottom:"20px"}}>
+        Escape from the fast-paced life to paradise awaited me at Sentosa Island, as it would for you too, a tropical haven brimming with endless possibilities for relaxation and adventure. Basking in the golden rays of the sun, I surrendered to the serenity of the pristine beaches, with their powdery sands and crystal-clear waters inviting me for a refreshing dip. After which, I visited the many other attractions, such as karting and SEA world!
+        </p>
+        <img src= "/sentosa.jpg" style={{width:'1200px', marginBottom:'80px'}}></img>
+        <p style={{marginBottom:"20px"}}>
+        Embarking on a journey through the vibrant attractions, I found myself whisked away on a thrilling adventure at Universal Studios Singapore, where heart-pounding rides and immersive experiences awaited at every turn. As dusk approached, I made my way to the iconic Sentosa Merlion, standing tall as a symbol of prosperity and good fortune, to witness a breathtaking sunset over the horizon, painting the sky in hues of crimson and gold.
+        </p>
+      </div>
+      <img src= "/universal-studios.jpg" style={{width:'1200px', marginBottom:'80px'}}></img>
+
+      <h2 style={{marginBottom:"50px", textAlign:"center",fontSize:"1.5rem"}}>
+        <strong>Pulau Ubin: A Utopia for Nature Lovers</strong>
+      </h2>
+
+      <div style={{wordSpacing: '5px' , fontSize:'1rem',textAlign: 'left',maxWidth: '1200px',marginBottom:'50px'}}>
+        <p style={{marginBottom:"20px"}}>
+        Tucked away from the bustling streets of Singapore lies the tranquil oasis of Pulau Ubin, a rustic island retreat steeped in natural beauty and serenity. Venturing across the shimmering waters to this idyllic haven, I found myself transported back in time to a bygone era of traditional kampung(village) life.</p>
+        <img src= "/palau-ubin-2.jpg" style={{width:'1200px', marginBottom:'80px'}}></img>
+        
+        <p style={{marginBottom:"20px"}}>
+        Renting a bicycle, I embarked on an exploration of the island's rugged terrain, meandering through lush mangrove forests, tranquil lakes, and quaint kampongs (villages). Along the way, I encountered an array of fascinating wildlife, from playful monkeys swinging through the trees to vibrant bird species soaring overhead. The highlight of my journey was reaching the summit of Puaka Hill, where I was rewarded with panoramic views of the surrounding islands and shimmering coastline. As I immersed myself in the island's untouched natural splendor, I couldn't help but marvel at the sense of peace and tranquility that pervaded every corner of Pulau Ubin, making it a hidden gem well worth discovering amidst the bustling metropolis of Singapore.
+        </p>
+      </div>
+
+      <img src= "/pulau-ubin.jpg" style={{width:'1200px', marginBottom:'80px'}}></img>
+      
+      <h2 style={{textAlign:"center",fontSize:"2rem",marginBottom:'80px'}}> 
+        <strong> A Myriad of Memories </strong>
+      </h2>
+
+      <div style={{wordSpacing: '5px' , fontSize:'1rem',textAlign: 'left',maxWidth: '1200px',marginBottom:'50px'}}>
+        <p style={{marginBottom:"20px"}}>
+        As a Singaporean, I am immensely proud to call this vibrant city-state my home, and I wholeheartedly recommend fellow travelers to embark on their own journey of discovery through the enchanting streets of Singapore. 
+        Whether you're drawn to the tranquil sanctuaries of Gardens by the Bay, the tropical and exciting vibes of Sentosa, or the rustic charm of Pulau Ubin, Singapore offers a variety of experiences waiting to be explored. 
+        Whether you're seeking urban marvels, tropical escapades, or serene retreats, Singapore has it all and more, so there's definitely something here to captivate every soul. So, pack your bags, set forth on an adventure, and let the Lion City weave its magic upon you. 
+        Singapore awaits, ready to enchant, inspire, and leave an indelible mark on your heart. Come, experience the wonders of my homeland, and create memories that will last a lifetime.
+        </p>
+      </div>
+
+      <h1 style={{fontSize:'2rem'}}>
+        <strong>Weather in Singapore</strong>
+      </h1>
+    </div>
+    </>
+  );
+}
