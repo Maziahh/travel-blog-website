ff57830f58000d41cf1aa22ddd4a8488e9b288e8
123456789pyl <151416081+123456789pyl@users.noreply.github.com>
2024-02-07 21:50:52 +0800

    Update layout.jsx
    comment

--- Singapore/layout.jsx
+++ Singapore/layout.jsx
@@ -1,16 +1,16 @@
 
 // layout.jsx
-import { Inter } from 'next/font/google'
-import Comment from './commentsection'
+import { Inter } from 'next/font/google' // Importing the Inter font from Google Fonts using Next.js's font module
+import Comment from './commentsection' // Importing the Comment component from the local file './commentsection'
 
-const inter = Inter({ subsets: ['latin'] });
+const inter = Inter({ subsets: ['latin'] }); // Initializing the Inter font with a subset of 'latin'
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>
-        {children}
-        <Comment/>
+        {children} {/* render children component */}
+        <Comment/> {/* render comment component */}
       </body>
     </html>
   );
