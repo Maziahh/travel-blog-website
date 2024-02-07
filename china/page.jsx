f9436d7f9f77c8fe45b8f0219456deff47300374
val <150682802+valerytyj@users.noreply.github.com>
2024-02-07 19:34:07 +0800

    Update china page.jsx

--- china/page.jsx
+++ china/page.jsx
@@ -1,1 +1,65 @@
+export default function china() {
+    return (
+      <>
+        <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', marginTop: '100px' }}>
+          <h1 style={{ fontSize: '50px', marginBottom: '50px' }}><strong>China</strong></h1>
+          <div style={{ wordSpacing: '5px', fontSize: '1rem', textAlign: 'left', maxWidth: '1200px', marginBottom: '80px' }}>
+            <p style={{ marginBottom: '20px' }}>
+              During my trip, I learnt more on China's culture and lifestyle. The experience was eye opening and a worthwhile one, where I also gain connections with the China students and students who were in the same trip as me. One of China's culture which I learn is they drink tea and family is one of their top priorities. The economy of the technology sector has also been growing at a rising rate, hence, Ngee Ann Polytechnic gave ICT students like me to go to Shenzhen to learn more on the trends and update of technology. For example, articficial intelligence is constantly developing in companies such as the one we visited, Tencent. Additionally, China has started using 5G network, enabling quick transfer. 🤯
+            </p>
+          </div>
+  
+          <img src="/valfriendsinchina.jpg" style={{ width: '1200px', marginBottom: '80px' }} />
+          <div style={{ wordSpacing: '5px', fontSize: '1rem', textAlign: 'left', maxWidth: '1200px', }}>
+            <p style={{ marginBottom: '20px' }}>
+             As mentioned above, I met and gained lifelong friendships with the China students and the students who were in the same trip as me. At the start of the trip, I only had a few acquaintances who I knew in the trip, however, the trip allowed me to step out of my comfort zone and strengthen our bonds with one another. We created great and memorable friendships, experiencing and learning new things. 
+            </p>
+            <p style={{ marginBottom: '50px' }}>
+            In the picture, my group and another group had a daily card playing session, such as poker and UNO. Even though we just met, we quickly bonded through the days we played with one another. 🥳
+            </p>
+          </div>
+  
+          <h2 style={{ marginBottom: "50px", textAlign: "center", fontSize: "1.5rem" }}>
+            <strong>Shenzhen: Silicon Valley of China</strong>
+          </h2>
+  
+          <div style={{ wordSpacing: '5px', fontSize: '1rem', textAlign: 'left', maxWidth: '1200px', marginBottom: '50px' }}>
+            <p style={{ marginBottom: "20px" }}>
+             The name "Silicon Valley of China" was given as Shenzhen is known as the major technology hub that is comparable to California's Silicon Valley in United States. Shenzhen is also recognized for its thriving technology industy, innovation ecosystem, and vibrant startup culture, making it a focal point for technology department and investment within China and globally. 
+            </p>
+            <p style={{ marginBottom: "20px" }}>
+              As you step foot into Shenzhen, china, you will be greeted with technological innovation and creativity. There are also many electronics markets such as Huaqiangbei, which I went with my friends on our free time to look for digital cameras. Both of us eventaully settled for one and brought the digital cameras home! In Shenzhen, there are Tech-themed parks and districts to doster innovation and technology development. For example, the Shenzhen High-Tech Industrial Park, which is a designated area for high-tech industries and research institutions. 💻 🖥️
+            </p>
+          </div>
+          <img src="/valgroupinchina.jpg" style={{ marginBottom: "50px", justifyContent: "center" }}></img>
+  
+          <h2 style={{ marginBottom: "50px", textAlign: "center", fontSize: "1.5rem" }}>
+            <strong>Exploring the cityscape of Shenzhen</strong>
+          </h2>
+  
+          <div style={{ wordSpacing: '5px', fontSize: '1rem', textAlign: 'left', maxWidth: '1200px', marginBottom: '50px' }}>
+            <p style={{ marginBottom: "20px" }}>
+              Through our exploration in Shenzhen, China, we went to multiple cultural visits such like "Splendid China", where one of the entertainments had different dynasties displayed in one of the performances. At the same area, me and my group mates went to a place that looked like a "Snow City" lookalike in Singapore. We had an extremely great time playing with the slides and snow inside. ❄️
+            </p>
+            <p style={{ marginBottom: "20px" }}>
+            In addition, we tried the food there and it was a life changing moment as the food served was not only cheap, but a big portion. No exploration of Shenzhen would be complete without indulging in its diverse culinary offerings. From street food stalls serving up local delicacies like steamed dumplings and grilled skewers to upscale restaurants showcasing Cantonese cuisine with a modern twist, the city is a paradise for food lovers. Don't miss the chance to sample seafood specialties at the bustling Dongmen Pedestrian Street or savor authentic dim sum at a traditional tea house. 🏯
+            </p>
+          </div>
+  
+          <h1 style={{ textAlign: "center", fontSize: "2rem", marginBottom: '80px' }}>
+            <strong> Here are photos to inspire you to visit Shenzhen, China! </strong>
+          </h1>
+  
+          <div style={{ justifyContent: 'center', width: '1000px', marginBottom: '50px' }}>
+            <img src="/chinalanterns.jpg" style={{ marginBottom: "20px", height: '900px', width: '1000px' }} />
+            <img src="/valinchina.jpg" style={{ width: '1000px' }} />
+          </div>
+  
+          <h1 style={{ fontSize: '2rem' }}>
+            <strong>Weather in Shenzhen, China</strong>
+          </h1>
+        </div>
+      </>
+    );
+  }
 
