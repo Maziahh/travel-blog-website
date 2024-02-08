c9ed842a81fd2ec0bc871b545bfc3d8e9830579d
123456789pyl <151416081+123456789pyl@users.noreply.github.com>
2024-02-08 21:31:15 +0800

    Update layout.jsx

--- usa/layout.jsx
+++ usa/layout.jsx
@@ -1,16 +1,16 @@
-
+// Yuet Lin
 // layout.jsx
 import { Inter } from 'next/font/google'
-import Comment from './commentsection'
+import Comment from './commentsection' // Importing the Comment component from the local file './commentsection'
 
-const inter = Inter({ subsets: ['latin'] });
+const inter = Inter({ subsets: ['latin'] }); 
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>
-        {children}
-        <Comment/>
+        {children} {/* Render children component */}
+        <Comment/> {/* Render comment component */}
       </body>
     </html>
   );
