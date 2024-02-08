fb05a4499086302b13a25b29f94ace81e50c15cf
Zoewewe <151327642+Zoewewe@users.noreply.github.com>
2024-02-08 23:03:08 +0800

    Delete destinations.jsx

--- destinations.jsx
+++
@@ -1,9 +0,0 @@
-//create a folder called destinations and then a file called page.jsx inside the destinations folder and put the code inside page.jsx
-
-export default function destinations() {
-    return (
-        <div>
-            <h1>destinations</h1>
-        </div>
-    );
-}
