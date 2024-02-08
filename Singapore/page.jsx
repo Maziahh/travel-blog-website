0e64395d1ad90c6d2704ecf22afd70f4040f5c08
123456789pyl <151416081+123456789pyl@users.noreply.github.com>
2024-02-08 09:01:21 +0800

    Update page.jsx
    comment

--- Singapore/page.jsx
+++ Singapore/page.jsx
@@ -2,45 +2,51 @@ export default function singapore() {
   return (
     <>
     <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', marginTop: '100px' }}>
-      <h1 style={{ fontSize: '50px', marginBottom: '50px' }}><strong>Singapore</strong></h1>
+      <h1 style={{ fontSize: '50px', marginBottom: '50px' }}><strong>Singapore</strong></h1> {/* header */}
       <div style={{ wordSpacing: '5px' , fontSize:'1rem',textAlign: 'left',maxWidth: '1200px',marginBottom:'80px'}}> 
+        {/* intro */}
         <p style={{marginBottom:'20px'}}>
         Singapore, a tiny island nation with a big heart, is a melting pot of cultures, cuisines, and experiences. Embarking on a journey through this vibrant city-state promises a kaleidoscope of adventures. Despite already living here, let me introduce to you some places around Singapore that may interest you! 
         </p>
       </div>
-
+      {/* garden by the bay paragraph title */}
       <h2 style={{marginBottom:"50px", textAlign:"center",fontSize:"1.5rem"}}>
         <strong>Gardens by the Bay: Nature within a City</strong>
       </h2>
 
+      {/* garden by the bay body */}
       <div style={{ wordSpacing: '5px' , fontSize:'1rem',textAlign: 'left',maxWidth: '1200px',}}> 
         <p style={{ marginBottom: '20px' }}>
         As I stepped into this urban oasis, I was transported into a surreal world where nature and technology harmoniously coexist. Towering supertrees adorned with intricate vertical gardens greeted me, casting a spellbinding allure. Walking along the elevated walkway, I marveled at the breathtaking panoramic views of the city skyline juxtaposed against lush greenery.       </p>
         <p style={{ marginBottom: '50px' }}>
         The iconic Flower Dome and Cloud Forest beckoned with their captivating displays of flora from around the globe, offering a tranquil sanctuary amidst the bustling cityscape. As night descended, I witnessed the garden come alive in a dazzling display of lights and music during the Garden Rhapsody, a spectacle that left me spellbound. </p>
       </div>
-
+      {/* garden by the bay image */}
       <img src= "/garden-by-the-bay.jpg" style={{width:'1200px', marginBottom:'80px'}}/> 
-
+      {/*sentosa header */}
       <h2 style={{marginBottom:"50px", textAlign:"center",fontSize:"1.5rem"}}>
         <strong>Sentosa Island: Where Discovery Never Ends</strong>
       </h2>
-
+      {/* sentosa body */}
       <div style={{wordSpacing: '5px' , fontSize:'1rem',textAlign: 'left',maxWidth: '1200px',marginBottom:'50px'}}>
         <p style={{marginBottom:"20px"}}>
         Escape from the fast-paced life to paradise awaited me at Sentosa Island, as it would for you too, a tropical haven brimming with endless possibilities for relaxation and adventure. Basking in the golden rays of the sun, I surrendered to the serenity of the pristine beaches, with their powdery sands and crystal-clear waters inviting me for a refreshing dip. After which, I visited the many other attractions, such as karting and SEA world!
         </p>
+        {/*sentosa img */}
         <img src= "/sentosa.jpg" style={{width:'1200px', marginBottom:'80px'}}></img>
         <p style={{marginBottom:"20px"}}>
         Embarking on a journey through the vibrant attractions, I found myself whisked away on a thrilling adventure at Universal Studios Singapore, where heart-pounding rides and immersive experiences awaited at every turn. As dusk approached, I made my way to the iconic Sentosa Merlion, standing tall as a symbol of prosperity and good fortune, to witness a breathtaking sunset over the horizon, painting the sky in hues of crimson and gold.
         </p>
       </div>
+      {/* universal studios img */}
       <img src= "/universal-studios.jpg" style={{width:'1200px', marginBottom:'80px'}}></img>
 
+      {/* pulau ubin header */}
       <h2 style={{marginBottom:"50px", textAlign:"center",fontSize:"1.5rem"}}>
         <strong>Pulau Ubin: A Utopia for Nature Lovers</strong>
       </h2>
 
+      {/* pulau ubin body */}
       <div style={{wordSpacing: '5px' , fontSize:'1rem',textAlign: 'left',maxWidth: '1200px',marginBottom:'50px'}}>
         <p style={{marginBottom:"20px"}}>
         Tucked away from the bustling streets of Singapore lies the tranquil oasis of Pulau Ubin, a rustic island retreat steeped in natural beauty and serenity. Venturing across the shimmering waters to this idyllic haven, I found myself transported back in time to a bygone era of traditional kampung(village) life.</p>
@@ -51,12 +57,15 @@ export default function singapore() {
         </p>
       </div>
 
+      {/* pulau ubin img */}
       <img src= "/pulau-ubin.jpg" style={{width:'1200px', marginBottom:'80px'}}></img>
       
+      {/* conclu header */}
       <h2 style={{textAlign:"center",fontSize:"2rem",marginBottom:'80px'}}> 
         <strong> A Myriad of Memories </strong>
       </h2>
 
+      {/* conclu body */}
       <div style={{wordSpacing: '5px' , fontSize:'1rem',textAlign: 'left',maxWidth: '1200px',marginBottom:'50px'}}>
         <p style={{marginBottom:"20px"}}>
         As a Singaporean, I am immensely proud to call this vibrant city-state my home, and I wholeheartedly recommend fellow travelers to embark on their own journey of discovery through the enchanting streets of Singapore. 
@@ -66,6 +75,7 @@ export default function singapore() {
         </p>
       </div>
 
+      {/* weather header */}
       <h1 style={{fontSize:'2rem'}}>
         <strong>Weather in Singapore</strong>
       </h1>
