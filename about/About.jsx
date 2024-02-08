8270ab608baab02ed566c2b4b6245b528dede2c7
val <150682802+valerytyj@users.noreply.github.com>
2024-02-09 06:39:09 +0800

    Create About.jsx

---
+++ about/About.jsx
@@ -0,0 +1,13 @@
+//About.jsx
+//valery
+import React from 'react';
+
+function AboutTitle() {
+    return (
+        <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', marginTop: '20px' }}>
+            <h1 style={{ fontSize: '50px', marginBottom: '10px' }}><strong>About us</strong></h1>
+        </div>
+    );
+}
+
+export default AboutTitle;
